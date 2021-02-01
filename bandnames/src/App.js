b17fdae816b476eb85e7911ff436260889a01641
bujosa <davidbujosa@gmail.com>
2021-02-01 00:27:33 -0400

    Installed socket.io

--- bandnames/src/App.js
+++ bandnames/src/App.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
+import io from 'socket.io-client';
 import { BandAdd } from './components/BandAdd';
 import { BandList } from './components/BandList';
 
+const connectSocketServer = () => {
+	return (
+		io.connect('http://localhost:8080'),
+		{
+			transports: ['websocket'],
+		}
+	);
+};
+
 function App() {
+	const [socket] = useState(connectSocketServer());
+	const [online, setOnline] = useState(false);
+
 	return (
 		<div className='container'>
 			<div className='alert'>
