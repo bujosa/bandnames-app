3233eb0afa300a867585db96479f9860a15a460f
bujosa <davidbujosa@gmail.com>
2021-02-01 02:34:36 -0400

    Created cusrtom hook

--- bandnames/src/App.js
+++ bandnames/src/App.js
@@ -1,34 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import io from 'socket.io-client';
 import { BandAdd } from './components/BandAdd';
 import { BandList } from './components/BandList';
-
-const connectSocketServer = () => {
-	const socket = io.connect('http://localhost:8080/');
-	return socket;
-};
+import { useSocket } from './hooks/useSocket';
 
 function App() {
-	const [socket] = useState(connectSocketServer());
-	const [online, setOnline] = useState(false);
 	const [bands, setBands] = useState([]);
 
-	useEffect(() => {
-		console.log(socket);
-		setOnline(socket.connected);
-	}, [socket]);
-
-	useEffect(() => {
-		socket.on('connect', () => {
-			setOnline(true);
-		});
-	}, [socket]);
-
-	useEffect(() => {
-		socket.on('disconnect', () => {
-			setOnline(false);
-		});
-	}, [socket]);
+	const { socket, online } = useSocket('http://localhost:8080');
 
 	useEffect(() => {
 		socket.on('current-bands', (bands) => {
